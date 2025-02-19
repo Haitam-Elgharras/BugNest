96b5482391c704e198148e8b21be67785151bbe2
haitam-elgharras <elgharras.haitam@gmail.com>
2025-02-19 22:54:11 +0100

    Add issue submission form

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@radix-ui/themes";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
   return (
-    <div>
-      <Button>IssuesPage</Button>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title" className="ps-2"></TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
     </div>
   );
 };
