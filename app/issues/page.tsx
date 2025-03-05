3c0f106af689063525edc464bcb411d606376524
haitam-elgharras <elgharras.haitam@gmail.com>
2025-03-05 22:31:59 +0000

    Add New Issue page and update Issues page navigation

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,13 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title" className="ps-2"></TextField.Root>
-      <TextArea placeholder="Description" />
-      <Button>Submit New Issue</Button>
+    <div>
+      <Button>
+        <Link href="issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+
 import { Theme } from "@radix-ui/themes";
 import "./globals.css";
 import Navbar from "./Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -27,10 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
+      <body className={inter.variable}>
+        <Theme accentColor="purple">
           <Navbar />
           <main className="p-5">{children}</main>
         </Theme>
